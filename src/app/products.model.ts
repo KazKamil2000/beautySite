71f90cd9f9df9101a24a7dc54b7183ec316436a2
KamilTheKoder <kamilkazmierczak0611@gmail.com>
2023-03-30 17:37:21 +0200

    im done for today

---
+++ src/app/products.model.ts
@@ -0,0 +1,12 @@
+export interface ProductsModel {
+    id: any,
+    name: string,
+    shortDesc: string,
+    desc: any,
+    when: any,
+    prosShort: any,
+    cons: any,
+    benefits: any,
+    example: any,
+    time: any
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import porductData from '../products.json';
+import { ProductsModel } from '../products.model';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+products: ProductsModel[] = porductData;
 }
